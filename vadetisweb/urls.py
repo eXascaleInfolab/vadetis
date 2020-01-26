d18099f35b2031604558c6ddbc6f679719fbab7e
adrian <adrian.haenni@gmail.com>
2020-01-26 18:39:02 +0100

    datasets views

--- vadetisweb/urls.py
+++ vadetisweb/urls.py
@@ -10,15 +10,15 @@ urlpatterns = [
     path('api/datasets/real-world', views.RealWorldDatasetsJson.as_view(), name='real_world_datasets_json'),
     path('api/datasets/synthetic', views.SyntheticDatasetsJson.as_view(), name='synthetic_datasets_json'),
 
-    path('datasets/real-world', views.RealWorldDatasets.as_view(), name='real_world_datasets'),
-    path('datasets/synthetic', views.SyntheticDatasets.as_view(), name='synthetic_datasets'),
+    path('datasets/real-world/', views.RealWorldDatasets.as_view(), name='real_world_datasets'),
+    path('datasets/synthetic/', views.SyntheticDatasets.as_view(), name='synthetic_datasets'),
 
     path('account/application-settings/', views.ApplicationSettings.as_view(), name='application_settings'),
-    path('account/datasets', views.account_datasets, name='account_datasets'),
-    path('account/datasets/upload', views.AccountUploadDataset.as_view(), name='account_datasets_upload'),
-    path('account/training-datasets', views.account_training_datasets, name='account_training_datasets'),
-    path('account/training-datasets/upload', views.AccountUploadTrainingDataset.as_view(), name='account_training_datasets_upload'),
-    path('account', views.account, name='account'),
+    path('account/datasets/', views.account_datasets, name='account_datasets'),
+    path('account/datasets/upload/', views.AccountUploadDataset.as_view(), name='account_datasets_upload'),
+    path('account/training-datasets/', views.account_training_datasets, name='account_training_datasets'),
+    path('account/training-datasets/upload/', views.AccountUploadTrainingDataset.as_view(), name='account_training_datasets_upload'),
+    path('account/', views.account, name='account'),
 
     path('', views.index, name='index'),
 
