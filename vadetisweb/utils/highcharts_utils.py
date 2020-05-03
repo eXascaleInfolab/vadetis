a4536a65779aea097c195f4f481b4e87643d75f6
adrian <adrian.haenni@gmail.com>
2020-05-03 19:26:53 +0200

    highcharts zoom preselected

--- vadetisweb/utils/highcharts_utils.py
+++ vadetisweb/utils/highcharts_utils.py
@@ -7,18 +7,20 @@ def get_highcharts_range_button_preselector(inferred_freq):
         return 2
 
     if inferred_freq.endswith(('B', 'C', 'D')): # freq of days
-        return 3 #week
-    elif inferred_freq.endswith(('W', 'M', 'SM', 'BM', 'CBM', 'MS', 'SMS', 'BMS', 'CBMS')): # freq of weeks & months
-        return 5 #year
+        return 3 # week
+    elif inferred_freq.endswith(('M', 'BM', 'CBM', 'MS', 'BMS', 'CBMS')): # freq of months
+        return 6 # all
+    elif inferred_freq.endswith(('W', 'SM', 'SMS')): # freq of weeks or two week
+        return 5 # year
     elif inferred_freq.endswith(('Q', 'BQ', 'QS', 'BQS')): # freq of quarters
-        return 5 #year
+        return 6 # all
     elif inferred_freq.endswith(('A', 'Y', 'BA', 'BY', 'AS', 'YS', 'BAS', 'BYS')): # freq of years
-        return 6 #all
+        return 6 # all
     elif inferred_freq.endswith(('BH', 'H')): # freq of hours
-        return 2 #day
+        return 3 # week
     elif inferred_freq.endswith(('T', 'min')):  # freq of minutes
-        return 1 #hour
+        return 1 # hour
     elif inferred_freq.endswith(('S')):  # freq of seconds
-        return 0 #minute
+        return 0 # minute
 
     return 2
