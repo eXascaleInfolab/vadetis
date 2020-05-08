51b87a12fb4058c791c52350d1692dbd62cfa6c3
adrian <adrian.haenni@gmail.com>
2020-05-08 11:21:24 +0200

    hide on reset dataset

--- vadetisweb/static/js/custom/highcharts.actions.init.js
+++ vadetisweb/static/js/custom/highcharts.actions.init.js
@@ -10,9 +10,18 @@ var VadetisHighchartsActionsReload = function () {
                 loadSeriesForType(highchart, url, "raw", true, function () {
                     isLoading = false;
                     button.html('Reload').removeClass('disabled');
+                    // TODO better hiding
+                    $('#threshold_portlet').hide();
+                    $('#scores_portlet').hide();
+                    $('#cnf_portlet').hide();
+                    $('#plot_portlet').hide();
                 });
             } else {
                 button.html('Reload').removeClass('disabled');
+                $('#threshold_portlet').hide();
+                $('#scores_portlet').hide();
+                $('#cnf_portlet').hide();
+                $('#plot_portlet').hide();
             }
         });
     };
