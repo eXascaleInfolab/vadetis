754ee098adf58771c191a48a3c650b6a3ad02005
adrian <adrian.haenni@gmail.com>
2020-05-15 15:27:52 +0200

    better injection part 2

--- vadetisweb/algorithms/injection.py
+++ vadetisweb/algorithms/injection.py
@@ -1,51 +1,88 @@
 import random
+import numpy as np
+
 from vadetisweb.utils import stochastic_duration
+from vadetisweb.utils import next_dt
+from vadetisweb.parameters import ANOMALY_TYPE_EXTREME, ANOMALY_TYPE_LEVEL_SHIFT, ANOMALY_TYPE_VARIANCE, ANOMALY_TYPE_TREND
 
-
-def anomaly_injection(dataset, conf):
+def anomaly_injection(dataset, validated_data):
 
     df = dataset.dataframe
     df_class = dataset.dataframe_class
 
     df_inject = dataset.dataframe.copy()
     df_inject_class = dataset.dataframe_class.copy()
-    time_series = dataset.timeseries_set.all()
+    #time_series = dataset.timeseries_set.all()
 
-    normal_lowerbound_duration = conf['normal_lowerbound_duration']
-    normal_upperbound_duration = conf['normal_upperbound_duration']
-    anomaly_lowerbound_duration = conf['anomaly_lowerbound_duration']
-    anomaly_upperbound_duration = conf['anomaly_upperbound_duration']
-    probability = conf['probability']
+    time_series = validated_data['time_series']
+    anomaly_type = validated_data['anomaly_type']
+    anomaly_factor = validated_data['anomaly_factor']
+    normal_lowerbound_duration = validated_data['normal_lowerbound_duration']
+    normal_upperbound_duration = validated_data['normal_upperbound_duration']
+    probability = validated_data['probability']
+    anomaly_lowerbound_duration = validated_data['anomaly_lowerbound_duration']
+    anomaly_upperbound_duration = validated_data['anomaly_upperbound_duration']
 
-    for ts in time_series:
-        done = False
-        normal_start_index = 0
-        anomaly_start_index = 0
 
-        while done is False:
-            normal_duration = stochastic_duration(normal_lowerbound_duration, normal_upperbound_duration)
-            anomaly_duration = stochastic_duration(anomaly_lowerbound_duration, anomaly_upperbound_duration)
+    done = False
+    normal_start_index = 0
+    anomaly_start_index = 0
 
-            # start of next anomaly duration
-            anomaly_start_index += normal_start_index + normal_duration
-            # start of next normal duration
-            normal_start_index += anomaly_start_index + anomaly_duration
+    while done is False:
+        normal_duration = stochastic_duration(normal_lowerbound_duration, normal_upperbound_duration)
+        anomaly_duration = stochastic_duration(anomaly_lowerbound_duration, anomaly_upperbound_duration)
 
-            # check break
-            if len(df.index) < anomaly_start_index:
-                done = True
-            else:
-                for index, value in df.loc[df.index[anomaly_start_index:normal_start_index], ts.id].iteritems(): # df.iloc[anomaly_start_index:normal_start_index, df.columns.get_loc(ts.id)].iteritems():
-                    # respect probability
-                    if random.uniform(0, 1) <= probability:
-                        # check if normal data at index -> change required
-                        if df_class.loc[index, ts.id] == False:
-                            inject_correlated_std_deviation_anomaly(df, df_inject, df_inject_class, index, ts.id)
+        # start of next anomaly duration
+        anomaly_start_index += normal_start_index + normal_duration
+        # start of next normal duration
+        normal_start_index += anomaly_start_index + anomaly_duration
+
+        # check break
+        if len(df.index) < anomaly_start_index:
+            done = True
+        else:
+            # respect probability
+            if random.uniform(0, 1) <= probability:
+
+                if anomaly_type == ANOMALY_TYPE_EXTREME:
+                    inject_extreme_outliers(df, df_inject, df_inject_class, anomaly_start_index, normal_start_index, time_series.id, anomaly_factor)
+
+                elif anomaly_type == ANOMALY_TYPE_LEVEL_SHIFT:
+                    inject_level_shift(df, df_inject, df_inject_class, anomaly_start_index, normal_start_index, time_series.id, anomaly_factor)
+
+                """elif anomaly_type == ANOMALY_TYPE_VARIANCE:
+
+                elif anomaly_type == ANOMALY_TYPE_TREND:"""
+
+
+
+                #inject_correlated_std_deviation_anomaly(df, df_inject, df_inject_class, index, time_series.id)
 
     return df_inject, df_inject_class
 
+def inject_extreme_outliers(df, df_inject, df_inject_class, anomaly_start_index, normal_start_index, ts_id, factor=10):
+    for index, value in df.loc[df.index[anomaly_start_index:normal_start_index], ts_id].iteritems():
+        df_inject.at[index, ts_id] = extreme_outlier(df, index, ts_id, factor)
+        df_inject_class.at[index, ts_id] = True
 
-#TODO change function
+def extreme_outlier(df, index, ts_id, factor=10):
+    before_dt = next_dt(index, 'earlier', df.index.inferred_freq, 10)
+    after_dt = next_dt(index, 'later', df.index.inferred_freq, 10)
+    local_std = df.loc[before_dt:after_dt, ts_id].std(axis=0, skipna=True, level=None, ddof=0)
+    return np.random.choice([-1, 1]) * factor * local_std
+
+def inject_level_shift(df, df_inject, df_inject_class, anomaly_start_index, normal_start_index, ts_id, factor=10):
+    before_dt = next_dt(df.index[anomaly_start_index], 'earlier', df.index.inferred_freq, 10)
+    after_dt = next_dt(df.index[normal_start_index], 'later', df.index.inferred_freq, 10)
+    local_std = df.loc[before_dt:after_dt, ts_id].std(axis=0, skipna=True, level=None, ddof=0)
+
+    multiplier = np.random.choice([-1, 1])
+    for index, value in df.loc[df.index[anomaly_start_index:normal_start_index], ts_id].iteritems():
+        df_inject.at[index, ts_id] += multiplier * factor * local_std
+        df_inject_class.at[index, ts_id] = True
+
+
+@DeprecationWarning
 def inject_correlated_std_deviation_anomaly(df, df_inject, df_inject_class, index, ts_id, multiplier=3):
     df_inject_class.at[index, ts_id] = True
     # ddof = 0: population standard deviation using n; ddof = 1: sample std deviation using n-1
