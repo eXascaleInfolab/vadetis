6ac2b6a9764d7db4e38abb7c1bc1774c70f8527a
adrian <adrian.haenni@gmail.com>
2020-05-23 22:31:50 +0200

    better form fields 2, color picker

--- vadetisweb/serializers/account_serializers.py
+++ vadetisweb/serializers/account_serializers.py
@@ -109,31 +109,31 @@ class UserSettingsSerializer(serializers.ModelSerializer):
                                            help_text='Default: #FF0000, the RGB color used to mark outliers',
                                            validators=[RegexValidator(regex='^#(?:[0-9a-fA-F]{3}){1,2}$')],
                                            style={'template': 'vadetisweb/parts/input/text_input.html',
-                                                  'input_class': 'minicolors-input'})
+                                                  'input_type': 'color'})
 
     color_clusters = serializers.CharField(max_length=7, default="#0000FF",
                                            help_text='Default: #0000FF, the RGB color used to mark LISA clusters of high or low values',
                                            validators=[RegexValidator(regex='^#(?:[0-9a-fA-F]{3}){1,2}$')],
                                            style={'template': 'vadetisweb/parts/input/text_input.html',
-                                                  'input_class': 'minicolors-input'})
+                                                  'input_type': 'color'})
 
     color_true_positive = serializers.CharField(max_length=7, default="#008800",
                                                 help_text='Default: #008800, the RGB color used to mark true positives',
                                                 validators=[RegexValidator(regex='^#(?:[0-9a-fA-F]{3}){1,2}$')],
                                                 style={'template': 'vadetisweb/parts/input/text_input.html',
-                                                       'input_class': 'minicolors-input'})
+                                                       'input_type': 'color'})
 
     color_false_positive = serializers.CharField(max_length=7, default="#FF0000",
                                                  help_text='Default: #FF0000, the RGB color used to mark false positives',
                                                  validators=[RegexValidator(regex='^#(?:[0-9a-fA-F]{3}){1,2}$')],
                                                  style={'template': 'vadetisweb/parts/input/text_input.html',
-                                                        'input_class': 'minicolors-input'})
+                                                        'input_type': 'color'})
 
     color_false_negative = serializers.CharField(max_length=7, default="#0000FF",
                                                  help_text='Default: #0000FF, the RGB color used to mark false negatives',
                                                  validators=[RegexValidator(regex='^#(?:[0-9a-fA-F]{3}){1,2}$')],
                                                  style={'template': 'vadetisweb/parts/input/text_input.html',
-                                                        'input_class': 'minicolors-input'})
+                                                        'input_type': 'color'})
 
     round_digits = serializers.IntegerField(default=3,
                                             help_text='Must be a number between 1 and 6',
