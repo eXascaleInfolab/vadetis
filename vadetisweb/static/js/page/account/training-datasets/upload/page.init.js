2c31fa81e4d2ba2a8320f3bcddf6fbcb957ed3a2
adrian <adrian.haenni@gmail.com>
2020-06-21 22:41:57 +0200

    upload pages

---
+++ vadetisweb/static/js/page/account/training-datasets/upload/page.init.js
@@ -0,0 +1,13 @@
+var AccountTrainingDatasetUploadPage = function () {
+
+    // private
+    var init = function (upload_form_id) {
+        initAjaxFormSubmit(upload_form_id, "json");
+    }
+    return {
+        // public
+        init: function(upload_form_id) {
+            init(upload_form_id);
+        }
+    };
+}();
