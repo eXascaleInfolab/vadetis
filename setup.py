5fdf2d0bf8f7274675296bb7329d1f30b250f64f
adrian <adrian.haenni@gmail.com>
2020-06-30 23:26:26 +0200

    better detection, cython dtw readded

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from distutils.core import setup
 from Cython.Build import cythonize
 import numpy
 
+# run: python3 setup.py build_ext --inplace
 setup(
     ext_modules = cythonize(['vadetis/*.pyx']),
     include_dirs = [numpy.get_include()]
