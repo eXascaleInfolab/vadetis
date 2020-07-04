5e44c042375bca9bfdeb9c44e51851fb8735499f
adrian <adrian.haenni@gmail.com>
2020-07-04 17:57:17 +0200

    level shift injector, wip

--- vadetisweb/algorithms/injector/trend_injector.py
+++ vadetisweb/algorithms/injector/trend_injector.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 import logging
 
 from .base import OutlierInjector
@@ -9,7 +8,7 @@ from vadetisweb.utils import next_earlier_dt, next_later_dt
 from vadetisweb.factory import msg_injection_all_anomalies
 
 
-class LevelShiftInjector(OutlierInjector):
+class TrendInjector(OutlierInjector):
 
     def __init__(self, validated_data):
         super().__init__(validated_data)
@@ -17,13 +16,13 @@ class LevelShiftInjector(OutlierInjector):
     def get_factor(self):
         anomaly_deviation = self.validated_data['anomaly_deviation']
         if anomaly_deviation == ANOMALY_INJECTION_DEVIATION_SMALL:
-            return 8
+            return 8 / 10
 
         elif anomaly_deviation == ANOMALY_INJECTION_DEVIATION_MEDIUM:
-            return 16
+            return 16 / 10
 
         elif anomaly_deviation == ANOMALY_INJECTION_DEVIATION_HIGH:
-            return 24
+            return 24 / 10
 
         elif anomaly_deviation == ANOMALY_INJECTION_DEVIATION_RANDOM:
             return np.random.choice([8, 16, 24])
@@ -63,35 +62,31 @@ class LevelShiftInjector(OutlierInjector):
         return np.random.choice([-1, 1]) * self.get_factor() * local_std
 
 
-    def next_injection_index(self, range_index):
+    def next_injection_index(self, range):
         """
-        :param range_index the range in which the anomaly is injected
+        :param range - the range in which the anomaly is injected
         :return: next index to insert anomaly
         """
-        if self.valid_time_range(range_indexes=range_index):
+        if self.valid_time_range(range_indexes=range):
             ts_id = self.get_time_series().id
-            df_normal_part = self.df_class.loc[(self.df_class.index.isin(range_index)) & (self.df_class[ts_id] == False), ts_id]
+            df_normal_part = self.df_class.loc[(self.df_class.index.isin(range)) & (self.df_class[ts_id] == False), ts_id]
             normal_indexes = df_normal_part.index
             inject_at_index = np.random.choice(normal_indexes)
             return inject_at_index
 
         return None
 
-
     def get_split_ranges(self):
         """
-        For level shift we consider only every second range in order to have some space between subsequent level shifts
+        For trend shift we consider only every second range in order to have some space between subsequent trends
         :return: the ranges to insert the anomaly into
         """
         split_ranges = super().get_split_ranges()
         return split_ranges[1:len(split_ranges):2] # numpy [start:stop:step]
 
-
-    def inject(self, range_index):
+    def inject(self, range):
         ts_id = self.get_time_series().id
-        inject_at_index = self.next_injection_index(range_index)
+        inject_at_index = self.next_injection_index(range)
         if inject_at_index is not None:
-            upper_boundary = min(next_later_dt(inject_at_index, self.df.index.inferred_freq, 10), self.df.index.max())
-            level_shit_indexes = pd.date_range(inject_at_index, upper_boundary, freq=self.df.index.inferred_freq)
-            self.df_inject.loc[level_shit_indexes, ts_id] += self.get_value(inject_at_index, ts_id)
-            self.df_inject_class.loc[level_shit_indexes, ts_id] = 1
+            self.df_inject.at[inject_at_index, ts_id] += self.get_value(inject_at_index, ts_id)
+            self.df_inject_class.at[inject_at_index, ts_id] = 1
