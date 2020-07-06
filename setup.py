b27ded6d4b3c838d72dbdae98cbd50a533d27789
adrian <adrian.haenni@gmail.com>
2020-07-06 21:56:07 +0200

    cythonize remove warning level by adding compiler directive

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ import numpy
 
 # run: python3 setup.py build_ext --inplace
 setup(
-    ext_modules = cythonize(['vadetisweb/anomaly_algorithms/detection/correleation/cutil/*.pyx']),
+    ext_modules = cythonize(['vadetisweb/anomaly_algorithms/detection/correleation/cutil/*.pyx'], compiler_directives={'language_level' : "3"}),
     include_dirs = [numpy.get_include()]
 )
