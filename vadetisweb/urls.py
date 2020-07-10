86e684fbc744187e9f3814ffa1891806a2a9d129
adrian <adrian.haenni@gmail.com>
2020-07-10 18:53:15 +0200

    about page

--- vadetisweb/urls.py
+++ vadetisweb/urls.py
@@ -85,5 +85,6 @@ urlpatterns = apipatterns + [
     path('account/', views.account, name='account'),
 
 
+    path('about/', views.about, name='about'),
     path('', views.index, name='index'),
 ]
--- vadetisweb/views/base_views.py
+++ vadetisweb/views/base_views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'vadetisweb/index.html',)
+
+def about(request):
+    return render(request, 'vadetisweb/about.html')
