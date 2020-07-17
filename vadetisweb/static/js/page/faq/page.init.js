fd570f5b6e1487f8016644b3ab628ad06cb81f4d
adrian <adrian.haenni@gmail.com>
2020-07-17 20:21:29 +0200

    faq 6

--- vadetisweb/static/js/page/faq/page.init.js
+++ vadetisweb/static/js/page/faq/page.init.js
@@ -2,9 +2,34 @@
 
 var FaqPage = function () {
 
+    var switchFaqContent = function (event, show_id, hide_ids) {
+        event.preventDefault();
+        hide_ids.forEach(id => $('#' + id).hide());
+        $('#' + show_id).show();
+    }
+
     // private
     var init = function () {
+        var hideIds = ['faq_general_content', 'faq_account_content', 'faq_dataset_content', 'faq_detection_content', 'faq_injection_content', 'faq_recommendation_content'];
 
+        $('#general_link').on('click', function (e) {
+            switchFaqContent(e, 'faq_general_content', hideIds);
+        });
+        $('#account_link').on('click', function (e) {
+            switchFaqContent(e, 'faq_account_content', hideIds);
+        });
+        $('#dataset_link').on('click', function (e) {
+            switchFaqContent(e, 'faq_dataset_content', hideIds);
+        });
+        $('#detection_link').on('click', function (e) {
+            switchFaqContent(e, 'faq_detection_content', hideIds);
+        });
+        $('#injection_link').on('click', function (e) {
+            switchFaqContent(e, 'faq_injection_content', hideIds);
+        });
+        $('#recommendation_link').on('click', function (e) {
+            switchFaqContent(e, 'faq_recommendation_content', hideIds);
+        });
 
     }
     return {
