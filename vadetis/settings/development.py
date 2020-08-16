f78a3a14638fbf4ce9ee3114c8562ff84cb4ffc4
adrian <adrian.haenni@gmail.com>
2020-08-16 15:14:58 +0200

    minor fixes

--- vadetis/settings/development.py
+++ vadetis/settings/development.py
@@ -13,11 +13,6 @@ DEBUG = True
 # allow all hosts during development
 ALLOWED_HOSTS = ['*']
 
-"""# adjust the minimal login
-LOGIN_URL = 'core_login'
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = 'core_login'"""
-
 # ##### DATABASE CONFIGURATION ############################
 
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
