c4e1dad071d2b2f7a6828b708321349cb966ad95
adrian <adrian.haenni@gmail.com>
2020-08-16 19:29:57 +0200

    working prod config done

--- vadetis/settings/common.py
+++ vadetis/settings/common.py
@@ -15,10 +15,10 @@ PROJECT_ROOT = dirname(DJANGO_ROOT)
 SITE_NAME = basename(DJANGO_ROOT)
 
 # collect static files here
-STATIC_ROOT = join(PROJECT_ROOT, 'run', 'static')
+STATIC_ROOT = join(PROJECT_ROOT, 'static')
 
 # collect media files here
-MEDIA_ROOT = join(PROJECT_ROOT, 'run', 'media')
+MEDIA_ROOT = join(PROJECT_ROOT, 'media')
 
 # look for static assets here
 STATICFILES_DIRS = [
