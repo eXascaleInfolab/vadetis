2aaa9b3eafab9958312be1cb60b0f7ea7a58abd3
adrian <adrian.haenni@gmail.com>
2020-08-26 14:25:25 +0200

    mysql install fix

--- vadetis_install.sh
+++ vadetis_install.sh
@@ -15,11 +15,10 @@ sudo systemctl enable mysql
 read -s -p "Enter MySQL ROOT Password: " mysql_password
 mysql_password="$mysql_password"
 # MySQL has sometimes a strange issue where root cannot login into a fresh installation (!!!)
-sudo mysql -u root -h localhost -e "
+sudo mysql -u root --password="$mysql_password" -h localhost -e "
 UPDATE mysql.user SET plugin='mysql_native_password' WHERE User='root';
 UPDATE mysql.user SET authentication_string=password('$mysql_password') where User='root';
-FLUSH PRIVILEGES;
-"
+FLUSH PRIVILEGES;"
 sudo systemctl mysql restart
 
 # CONFIGURATION
